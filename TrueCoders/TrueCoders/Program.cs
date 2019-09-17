ffa118cb672b0e3358417ec22f42e8c2b1ea4df7
cdoyle02 <54860089+cdoyle02@users.noreply.github.com>
2019-09-17 13:28:05 -0500

    Add files via upload

---
+++ TrueCoders/TrueCoders/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TrueCoders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            People a = new People(25, "Charlie", "Doyle", "Black");
+            People b = new People(32, "Michael", "Doyle", "Black");
+            People c = new People(25, "Graham", "Spencer", "Black");
+
+            List<People> AH = new List<People>();
+
+            AH.Add(a);
+            AH.Add(b);
+            AH.Add(c);
+
+            foreach(var person in AH)
+            {
+                Console.WriteLine($"Name: {person.FirstName} {person.LastName} Age: {person.Age} Hair Color: {person.HairColor}");
+            }
+           
+        }
+    }
+}
